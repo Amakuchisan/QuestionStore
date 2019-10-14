cf033aaffccf916cb62e288c286b8cde36539748
amakuchi <s18t318@stu.kagawa-u.ac.jp>
2019-10-14 21:26:20 +0900

    set cookie

--- handler/handler.go
+++ handler/handler.go
@@ -2,11 +2,18 @@ package handler
 
 import (
 	"github.com/labstack/echo"
+	"github.com/stretchr/objx"
 	"net/http"
 )
 
 // MainPage -- top page
 func MainPage(c echo.Context) error {
+	auth, err := c.Cookie("auth")
+	if err != nil {
+		return c.Redirect(http.StatusMovedPermanently, "/auth/login/google")
+	}
+	userData := objx.MustFromBase64(auth.Value)
+
 	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
 		"name": "Dolly!",
 	})
