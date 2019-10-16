e39911e34652c1af27ab749368051ac987bce80e
amakuchi <s18t318@stu.kagawa-u.ac.jp>
2019-10-16 23:20:51 +0900

    implement /question

---
+++ repository/question.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"github.com/Amakuchisan/QuestionBox/model"
+	"github.com/jmoiron/sqlx"
+	// "log"
+)
+
+// QuestionModel -- have method about user table in database
+type QuestionModel struct {
+	db *sqlx.DB
+}
+
+// NewQuestionModel QuestionModelの初期化
+func NewQuestionModel(db *sqlx.DB) *QuestionModel {
+	return &QuestionModel{db: db}
+}
+
+// All -- SELECT * FROM questions
+func (q *QuestionModel) All() ([]model.Question, error) {
+	questions := []model.Question{}
+	err := q.db.Select(&questions, "SELECT * from question")
+	if err != nil {
+		return nil, err
+	}
+	return questions, nil
+}
